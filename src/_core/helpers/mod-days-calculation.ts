f2f79b76dbcf857372c6f31c3eb4aa6c18354f2b
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-04-01 18:14:05 +0300

    loss entity calculation base logic

---
+++ src/_core/helpers/mod-days-calculation.ts
@@ -0,0 +1,37 @@
+import {
+  CalculatedData,
+  CalculatedIncrement,
+  DataFlat,
+  DayResultData,
+  EntityLossFlat,
+  EntityNamesEnum,
+} from "../models/loss-entities";
+
+export function calculateDaysData(
+  inputData: DataFlat,
+  averageData: CalculatedData,
+  summaryData: CalculatedData
+): DataFlat {
+  return inputData.map((dayResult) => {
+    const { data } = dayResult;
+    const updatedData: DayResultData = Object.fromEntries(
+      Object.entries(data).map(([key, entityLoss]) => {
+        const keyName = key as EntityNamesEnum;
+        const averageDataForEntity = averageData[keyName];
+        const summaryDataForEntity = summaryData[keyName];
+        const calculatedIncrement: CalculatedIncrement = {
+          comparedToAverage: averageDataForEntity !== 0 ? +(entityLoss.increment / averageDataForEntity).toFixed(1) : 0,
+          diffWithAverage: entityLoss.increment - averageDataForEntity,
+          average: averageDataForEntity,
+          summary: summaryDataForEntity,
+        };
+        const updatedEntityLoss: EntityLossFlat = {
+          ...entityLoss,
+          calculatedIncrement,
+        };
+        return [keyName, updatedEntityLoss];
+      })
+    ) as DayResultData;
+    return { ...dayResult, data: updatedData };
+  });
+}
--- src/app/components/entity-selection/entity-selection.tsx
+++ src/app/components/entity-selection/entity-selection.tsx
@@ -1,14 +1,14 @@
 import { DynamicIcon } from "@/components/dynamic-icon/dynamic-icon";
-import { ENTITIES_MAP } from "@/models/loss-entities";
+import { ENTITIES_MAP } from "@/_core/models/loss-entities";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 export function EntitySelection() {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-row flex-wrap gap-3">
       {Object.entries(ENTITIES_MAP).map(([category, entities]) => {
         return (
-          <div key={category} className="flex flex-row ">
+          <div key={category} className="flex flex-row">
             <ToggleButtonGroup aria-label="entities">
               {entities.map((entity) => {
                 return (
