cce7895d5480d7a3e71c40a150998ac241acc425
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-04-16 18:00:22 +0300

    cache invalidation

--- src/_core/data/fetch-data.ts
+++ src/_core/data/fetch-data.ts
@@ -1,9 +1,10 @@
-import { DayResultFlat, RussianLossesData } from '../models/loss-entities';
+import { DayResultFlat, RussianLossesData } from "../models/loss-entities";
 
+const SECONDS_IN_HOUR = 3600;
 export async function fetchData(): Promise<RussianLossesData> {
-  const response = await fetch(`${process.env.API_URL}/api/mod?flat=true`);
+  const response = await fetch(`${process.env.API_URL}/api/mod?flat=true`, { next: { revalidate: SECONDS_IN_HOUR } });
   if (!response.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
   const data: Array<DayResultFlat> = await response.json();
   return data.map((item) => ({ data: item.data, date: new Date(item.date), dayOfInvasion: item.dayOfInvasion }));
