0a22f5459bc1763e8cf68db1e252aee3ebdcb709
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-04-17 15:41:52 +0300

    minor layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-start items-stretch w-full h-full overflow-hidden">
       <Header />
-      <Paper elevation={1} className="sm:p-8 p-2 flex flex-col justify-start items-center m-2">
+      <Paper elevation={1} className="sm:p-8 p-2 flex flex-col justify-start items-center sm:m-2">
         <DisplayConfigurationSelection />
       </Paper>
-      <Paper elevation={1} className="sm:p-8 p-2 flex-1 flex flex-col justify-start items-stretch m-2">
+      <Paper elevation={1} className="sm:p-8 p-2 flex-1 flex flex-col justify-start items-stretch sm:m-2">
         <div className="w-100 flex-1 flex flex-col justify-start items-stretch" style={{ minHeight: "60vh" }}>
           <DataContainer />
         </div>
